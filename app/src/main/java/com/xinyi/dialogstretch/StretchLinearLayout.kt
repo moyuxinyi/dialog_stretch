b65c20961f4224420144e909eb275425d69f4136
xinyi <2420378389@qq.com>
2025-06-27 16:15:14 +0800

    🗑 remove: 删除无用代码

--- app/src/main/java/com/xinyi/dialogstretch/StretchLinearLayout.kt
+++ app/src/main/java/com/xinyi/dialogstretch/StretchLinearLayout.kt
@@ -3,7 +3,6 @@ package com.xinyi.dialogstretch
 import android.animation.ValueAnimator
 import android.content.Context
 import android.util.AttributeSet
-import android.view.View
 import android.widget.LinearLayout
 
 /**
