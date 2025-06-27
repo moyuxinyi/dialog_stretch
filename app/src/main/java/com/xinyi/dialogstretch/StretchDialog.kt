649f24fa00f0773de805d901b2577390673b795c
xinyi <2420378389@qq.com>
2025-06-27 16:11:04 +0800

    🗑 remove: 删除无用代码

--- app/src/main/java/com/xinyi/dialogstretch/StretchDialog.kt
+++ app/src/main/java/com/xinyi/dialogstretch/StretchDialog.kt
@@ -1,12 +1,10 @@
 package com.xinyi.dialogstretch
 
-import android.animation.ValueAnimator
 import android.app.Dialog
 import android.content.Context
 import android.os.Handler
 import android.os.Looper
 import android.view.Gravity
-import android.view.View
 import android.view.ViewGroup
 import android.view.WindowManager
 import androidx.core.graphics.drawable.toDrawable
@@ -89,33 +87,6 @@ class StretchDialog(context: Context) : Dialog(context, R.style.BaseDialogTheme)
         binding.tvParticularsOk.setOnClickListener {
             dismiss()
         }
-    }
-
-    /**
-     * 通过属性动画平滑地改变指定 View 的高度，实现拉伸或收缩动画效果
-     *
-     * @param view 需要调整高度的目标 View，一般是容器布局
-     * @param targetHeight 目标高度，单位 px，动画将从当前高度变到此高度
-     * @param duration 动画时长，单位毫秒，默认300ms
-     */
-    private fun animateHeightChange(view: View, targetHeight: Int, duration: Long = 300L) {
-        // 获取当前View的高度，作为动画起始高度
-        val startHeight = view.height
-
-        // 创建一个整数属性动画，从起始高度到目标高度
-        val animator = ValueAnimator.ofInt(startHeight, targetHeight).apply {
-            this.duration = duration
-            // 每帧动画更新时，获取当前动画值，动态设置View高度
-            addUpdateListener { animation ->
-                val animatedValue = animation.animatedValue as Int
-                val layoutParams = view.layoutParams
-                layoutParams.height = animatedValue
-                view.layoutParams = layoutParams
-            }
-        }
-
-        // 启动动画
-        animator.start()
     }
 
     override fun dismiss() {
